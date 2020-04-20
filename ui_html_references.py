b29df17ac95e9616ebf5e50ee52f4eaa6a57501c
Nathaniel Nethercott <nathanielnethercott@Nathaniels-MacBook-Pro.local>
2020-04-20 19:26:27 -0400

    added funtionality to lookup stocks during a specific time frame and started handling ui interactions with Yahoo finance

---
+++ ui_html_references.py
@@ -0,0 +1,11 @@
+"""
+css identifiers for ui things such as buttons or dropdown menus 
+"""
+
+history_locations = {
+'dropdown':"svg[class='Va(m)! Mstart(8px) Stk($linkColor) Fill($linkColor) dateRangeBtn:h_Fill($linkActiveColor) dateRangeBtn:h_Stk($linkActiveColor) W(8px) H(8px) Cur(p)']",
+'startDate':"input[name='startDate']",
+'endDate':"input[name='endDate']",
+'Done':"button[class=' Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Miw(80px)! Fl(start)']",
+'Apply':"button[class=' Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Fl(end)']"
+}
