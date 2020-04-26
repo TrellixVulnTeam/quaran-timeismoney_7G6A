ac94b205eed5054ef169b7c5fe2a76461bc8a627
Nathaniel Nethercott <nathanielnethercott@Nathaniels-MacBook-Pro.local>
2020-04-25 21:21:53 -0400

    not sure if everything here is up to snub so stick with dynamics2

---
+++ portfolio.py
@@ -0,0 +1,39 @@
+from stock import stock
+import time
+
+class portfolio():
+    def __init__(self, portfolio_contents, driver):
+        self.portfolio_ = portfolio_contents
+        self.driver = driver
+        self.driver.launch()
+
+    def netAssetValue(self):
+        current_positions = []
+        for item in self.portfolio_:
+            stock_obj = stock(item['ticker'],self.driver)
+            time.sleep(2)
+            current_positions.append(stock_obj.get_current())
+
+        original_positions = [item['entry_price'] for item in self.portfolio_]
+
+        self.netValue = 0
+        for index, pair in enumerate(zip(current_positions, original_positions)):
+            self.netValue += (pair[0]-pair[1])*self.portfolio_[index]['quantity']
+
+
+
+'''positions_current = []
+positions_original = []
+for item in Portfolio:
+    positions_original.append(item['entry_price'])
+    a = stock(item['ticker'])
+    positions_current.append(a.get_current())
+
+#clean this whole process up a little
+net = 0
+index = 0
+for current, original in zip(positions_current, positions_original):
+    delta = current-original
+    net+=delta*Portfolio[index]['quantity']
+    index+=1
+'''
