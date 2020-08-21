a2737f1dfea0c8987f75bfe719679940762e6adc
Nathaniel Nethercott <nathanielnethercott@Nathaniels-MacBook-Pro.local>
2020-08-21 13:07:28 -0400

    changing things manually cause git sucks

--- portfolio.py
+++ portfolio.py
@@ -1,39 +1,36 @@
 from stock import stock
 import time
+import datetime
 
 class portfolio():
-    def __init__(self, portfolio_contents, driver):
+    def __init__(self, portfolio_contents):
         self.portfolio_ = portfolio_contents
-        self.driver = driver
-        self.driver.launch()
+        self.netAssetValue()
+        self.weekly()
 
     def netAssetValue(self):
+        self.stocks = []
         current_positions = []
         for item in self.portfolio_:
-            stock_obj = stock(item['ticker'],self.driver)
+            stock_obj = stock(item['ticker'])
+            stock_obj.get_history()
             time.sleep(2)
             current_positions.append(stock_obj.get_current())
+            stock_obj.driver.terminate()
+            self.stocks.append(stock_obj)
 
         original_positions = [item['entry_price'] for item in self.portfolio_]
 
         self.netValue = 0
         for index, pair in enumerate(zip(current_positions, original_positions)):
-            self.netValue += (pair[0]-pair[1])*self.portfolio_[index]['quantity']
+            running_net = (pair[0]-pair[1])*self.portfolio_[index]['quantity']
+            self.stocks[index].delta = running_net
+            self.netValue += running_net
 
-
-
-'''positions_current = []
-positions_original = []
-for item in Portfolio:
-    positions_original.append(item['entry_price'])
-    a = stock(item['ticker'])
-    positions_current.append(a.get_current())
-
-#clean this whole process up a little
-net = 0
-index = 0
-for current, original in zip(positions_current, positions_original):
-    delta = current-original
-    net+=delta*Portfolio[index]['quantity']
-    index+=1
-'''
+    def weekly(self):
+        #date = datetime.date.today().strftime("%B %d, %Y") might not need this
+        for index, company in enumerate(self.stocks):
+            prev_open = company.history.iloc[6]['Open']
+            current_close = company.history.iloc[0]['Adj. Close']
+            delta = self.portfolio_[index]['quantity']*(current_close - prev_open)
+            company.weeklyDelta = delta
